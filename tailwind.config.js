ab76ff194b8c632c6149a72b127536842268f762
FlorPM <florpm1991@gmail.com>
2024-02-19 16:18:21 +0100

    🎨 Add styles and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      primary: 'Poppins',
+      secondary: 'Archivo-Black'
+    },
+    container: {
+      padding: {
+        DEFAULT: '30px',
+        lg: '0'
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primary: '#222222',
+        secondary: '#E5B7E2',
+        terciary: '#663B36'
+      },
+      backgroundImage: {
+        hero: "url('./assets/bg.svg')"
+      }
+    }
   },
   plugins: []
 }
