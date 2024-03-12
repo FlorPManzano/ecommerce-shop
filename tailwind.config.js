2c2091e209baaa07c0b9cc0dda93b4f7c90402f6
FlorPM <florpm1991@gmail.com>
2024-03-12 15:47:07 +0100

    🔠 Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,30 +4,30 @@ export default {
   theme: {
     fontFamily: {
       primary: 'Poppins',
-      secondary: 'Archivo-Black'
+      secondary: 'Archivo Black',
     },
     container: {
       padding: {
         DEFAULT: '30px',
-        lg: '0'
-      }
+        lg: '0',
+      },
     },
     screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
-      xl: '1440px'
+      xl: '1440px',
     },
     extend: {
       colors: {
         primary: '#222222',
         secondary: '#E5B7E2',
-        terciary: '#663B36'
+        terciary: '#663B36',
       },
       backgroundImage: {
-        hero: "url('./assets/bg.svg')"
-      }
-    }
+        hero: "url('./assets/bg.svg')",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
