838b7794bdbbbf86d2ba3ee5cf7dda81392bea28
FlorPM <florpm1991@gmail.com>
2024-03-18 18:00:51 +0100

    ⬆ Add Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,27 @@
+import { Link } from 'react-router-dom'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
 const Footer = () => {
   return (
-        <div>Footer</div>
+    <footer className='bg-primary py-12'>
+      <div className='container mx-auto'>
+        <p className='text-white text-center'>
+          Ecommerce 2024 realizado con React y TailwindCSS. <br /> Hecho con
+          cariño por Flor ♥
+        </p>
+        <div className='flex items-center justify-center gap-x-2 mt-4'>
+          <Link
+            to='https://github.com/FlorPManzano/ecommerce-shop'
+            target='_blank'
+          >
+            <FaGithub className='text-white text-2xl' />
+          </Link>
+          <Link to='https://www.linkedin.com/in/florpmanzano/' target='_blank'>
+            <FaLinkedin className='text-white text-2xl' />
+          </Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
